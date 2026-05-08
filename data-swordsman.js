c7cb258714e63b8e23fabd7657627698efccb225
Anntero <74142176+Anntero@users.noreply.github.com>
2026-05-08 09:34:25 +0500

    Create data-swordsman.js

---
+++ data-swordsman.js
@@ -0,0 +1,6 @@
+allSkills.push(
+    { "tree": "Swordsman Tree", "name": "Thrust", "stats": "CD: 0s", "description": "Rapid stabs. No Cooldown." },
+    { "tree": "Highlander Tree", "name": "Cartar Stroke", "stats": "CD: 20s", "description": "Charge attack. Charging time removed." },
+    { "tree": "Barbarian Tree", "name": "Cleave", "stats": "CD: 0s", "description": "Slash attack. Increases Crit Rate. No Cooldown." },
+    { "tree": "Peltasta Tree", "name": "Swash Buckling", "stats": "CD: 25s", "description": "Attracts nearby monsters to attack you." }
+);
