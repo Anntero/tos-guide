3d41d367446d7dd02d1953cb81eca47ae92ae2b4
Anntero <74142176+Anntero@users.noreply.github.com>
2026-05-08 09:35:46 +0500

    Update data-swordsman.js

--- data-swordsman.js
+++ data-swordsman.js
@@ -1,6 +1,14 @@
+// Swordsman Data Part 1
 allSkills.push(
-    { "tree": "Swordsman Tree", "name": "Thrust", "stats": "CD: 0s", "description": "Rapid stabs. No Cooldown." },
-    { "tree": "Highlander Tree", "name": "Cartar Stroke", "stats": "CD: 20s", "description": "Charge attack. Charging time removed." },
-    { "tree": "Barbarian Tree", "name": "Cleave", "stats": "CD: 0s", "description": "Slash attack. Increases Crit Rate. No Cooldown." },
-    { "tree": "Peltasta Tree", "name": "Swash Buckling", "stats": "CD: 25s", "description": "Attracts nearby monsters to attack you." }
+    { "tree": "Swordsman Tree", "name": "Thrust", "stats": "CD: 0s", "description": "Rapid stabs. Deals extra damage to bleeding targets. ★ No Cooldown." },
+    { "tree": "Swordsman Tree", "name": "Bash", "stats": "OH: 3 | CD: 8s", "description": "Strike that can knock down enemies." },
+    { "tree": "Swordsman Tree", "name": "Gung Ho", "stats": "CD: 0s", "description": "Increases Attack at the cost of Defense. ★ Permanent buff." },
+    { "tree": "Highlander Tree", "name": "Cartar Stroke", "stats": "CD: 20s", "description": "Massive strike. ★ Charging time removed." },
+    { "tree": "Highlander Tree", "name": "Cross Guard", "stats": "CD: 0s", "description": "Blocks with 2H Sword. Makes enemy vulnerable to Pierce. ★ No Cooldown." },
+    { "tree": "Peltasta Tree", "name": "Swash Buckling", "stats": "CD: 25s", "description": "Attracts nearby monsters to attack you." },
+    { "tree": "Peltasta Tree", "name": "Shield Lob", "stats": "CD: 0s", "description": "Throws shield at enemies. ★ No Cooldown." },
+    { "tree": "Barbarian Tree", "name": "Cleave", "stats": "CD: 0s", "description": "Slash attack. Increases Crit Rate. ★ No Cooldown." },
+    { "tree": "Barbarian Tree", "name": "Seism", "stats": "OH: 3 | CD: 18s", "description": "Slams the ground to deal damage to multiple enemies." },
+    { "tree": "Hoplite Tree", "name": "Finestra", "stats": "CD: 0s", "description": "Increases Crit Rate and Block while using a spear. ★ Permanent buff." },
+    { "tree": "Hoplite Tree", "name": "Stabbing", "stats": "CD: 25s", "description": "Repeated spear stabs." }
 );
